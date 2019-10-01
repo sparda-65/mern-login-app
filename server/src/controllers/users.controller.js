581b2f1b691526441b67e54c26370120fe40b26b
Abdellah Chahboune <abdellah_chahboune@inwi.ma>
2019-10-01 15:38:35 +0100

    add @hapi/joi validation and register controller

--- server/src/controllers/users.controller.js
+++ server/src/controllers/users.controller.js
@@ -1,13 +1,43 @@
 const User= require('../models/user.model.js');
+const bcrypt = require('bcryptjs');
+const {registerValidation} = require('../actions/validation');
 const userController ={};
 
 /**
  * Sing up Logic
  */
-userController.register = (req, res, next) => {
-    res.send({
-        message :` Sing Up`
+userController.register = async (req, res, next) => {
+    //User validation befor Save
+    const {error} = registerValidation(req.body);
+    if(error){
+        return res.status(400).send(error.details[0].message);
+    }
+
+    // check if e-mail existe on DB
+    const emailexist= await User.findOne({email:req.body.email});
+    if(emailexist) return res.status(400).send('email already existe');
+
+    // check if username existe on DB
+    const usernameexist= await User.findOne({username:req.body.username});
+    if(usernameexist) return res.status(400).send('username already existe');
+
+    //Hash the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    const user = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: hashedPassword
     });
+    
+    // save user on DB
+    try {
+        const savedUser = await user.save();
+        res.send({user: user._id}); 
+    } catch (error) {
+        res.status(400).send(error);
+    }
 };
 
 module.exports = userController;
