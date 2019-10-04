0b1be4dc4938cb81615df27a9995faab5b38f487
sparda_65 <chahboune.abdellah@gmail.com>
2019-10-04 17:52:13 +0100

    add components login navbar home to client

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import { Button } from 'reactstrap';
+import {Route} from 'react-router-dom';
+import {Container} from 'reactstrap';
+
+import {Home, Login} from './pages';
+import {NavBar} from './components'
 
 function App() {
   return (
     <div>
-      <h1> Hello react</h1>
-      <Button color="danger">Danger!</Button>
+      <Container>
+      <NavBar/>
+      <Route path='/' component ={Home} exact/>
+      <Route path='/Login' component ={Login} exact/>
+      </Container>
     </div>
   );
 }
