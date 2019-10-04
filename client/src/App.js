727406396bdf4af5ba1f6e386b7d048b9d353c61
sparda_65 <chahboune.abdellah@gmail.com>
2019-10-04 16:04:52 +0100

    add react js in client folder

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from 'reactstrap';
+
+function App() {
+  return (
+    <div>
+      <h1> Hello react</h1>
+      <Button color="danger">Danger!</Button>
+    </div>
+  );
+}
+
+export default App;
