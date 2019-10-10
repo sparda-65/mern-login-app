109b6b775ce1560ec65a8f11a4c1832a6977a67d
sparda-65 <chahboune.abdellah@gmail.com>
2019-10-10 07:51:37 +0100

    modifie NavBar and connect it to store

--- client/src/components/NavBar.component.js
+++ client/src/components/NavBar.component.js
@@ -1,5 +1,7 @@
-import React,{ Component } from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
 import {
   Collapse,
   Navbar,
@@ -11,36 +13,70 @@ import {
   UncontrolledDropdown,
   DropdownToggle,
   DropdownMenu,
-  DropdownItem } from 'reactstrap';
+  DropdownItem,
+  ButtonDropdown,
+} from 'reactstrap';
 
-class NavBar extends Component {
+class NavBarComponent extends Component {
   constructor(props) {
     super(props);
 
     this.toggle = this.toggle.bind(this);
+    this.toggleButton=this.toggleButton.bind(this);
+
     this.state = {
-      isOpen: false
+      isOpen: false,
+      dropdownOpen: false
     };
+
   }
+
   toggle() {
     this.setState({
       isOpen: !this.state.isOpen
     });
   }
+
+  toggleButton(){
+    this.setState({
+      dropdownOpen: !this.state.dropdownOpen
+    });
+  };
+
+  _renderLoginOrLogout() {
+    const {isAuth}=this.props;
+    if(isAuth){
+      return(
+        <ButtonDropdown isOpen={this.state.dropdownOpen} toggle={this.toggleButton}>
+        <DropdownToggle caret color="link" size="sm">
+          Button Dropdown
+        </DropdownToggle>
+        <DropdownMenu>
+          <DropdownItem header>Header</DropdownItem>
+          <DropdownItem disabled>Action</DropdownItem>
+          <DropdownItem>Another Action</DropdownItem>
+          <DropdownItem divider />
+          <DropdownItem>Another Action</DropdownItem>
+        </DropdownMenu>
+      </ButtonDropdown>
+      );
+    }
+
+    return (
+      <NavItem>
+        <Link to="/login" className="nav-link">Login</Link>
+      </NavItem>
+    );
+  };
   render() {
     return (
       <div>
         <Navbar color="dark" dark expand="md">
-        <Link to="/" className="navbar-brand">Mern Login App</Link>
+          <Link to="/" className="navbar-brand">Mern Login App</Link>
           <NavbarToggler onClick={this.toggle} />
           <Collapse isOpen={this.state.isOpen} navbar>
             <Nav className="ml-auto" navbar>
-              <NavItem>
-              <Link to="/login" className="nav-link">Login</Link>
-              </NavItem>
-              <NavItem>
-              <Link to="/signup" className="nav-link">Sing Up</Link>
-              </NavItem>
+              {this._renderLoginOrLogout()}
             </Nav>
           </Collapse>
         </Navbar>
@@ -49,4 +85,11 @@ class NavBar extends Component {
   }
 }
 
-export  { NavBar };
+const mapStateToProps = ({ auth }) => {
+  return {
+    isAuth: auth.isAuth
+  };
+};
+const NavBar = connect(mapStateToProps)(NavBarComponent);
+
+export { NavBar };
